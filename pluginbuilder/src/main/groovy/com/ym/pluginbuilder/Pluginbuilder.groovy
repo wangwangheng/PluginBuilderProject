a1126b7885e741d09e8aa6a52386bc3085e773b1
yiming <18720625976@163.com>
2016-11-25 17:06:54 +0800

    练手

--- pluginbuilder/src/main/groovy/com/ym/pluginbuilder/Pluginbuilder.groovy
+++ pluginbuilder/src/main/groovy/com/ym/pluginbuilder/Pluginbuilder.groovy
@@ -1,6 +1,5 @@
 package com.ym.pluginbuilder
 
-import groovy.lang.Closure
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -13,7 +12,13 @@ import org.gradle.api.Project
  * 修改时间：2016/11/24 15:58
  * 修改备注：
  *
+ * 官方教程：https://docs.gradle.org/current/userguide/custom_plugins.html
  * 教程：http://kvh.io/cn/embrace-android-studio-gradle-plugin.html
+ * android gradle build :https://android.googlesource.com/platform/tools/build/+/729497f/gradle/src/main/groovy/com/android/build/gradle/AndroidPlugin.groovy
+ * android gradle build : https://github.com/google/android-gradle-dsl
+ * http://google.github.io/android-gradle-dsl/javadoc/
+ *
+ * http://blog.csdn.net/qinxiandiqi/article/details/37925629
  */
 
 public class Pluginbuilder implements Plugin<Project> {
@@ -21,36 +26,30 @@ public class Pluginbuilder implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-
+        Boolean isApp = true;
         project.extensions.create('apkdistconf', ApkDistExtension);
+        //只可以在 android application 或者 android lib 项目中使用
+        if (!project.android) {
+            throw new IllegalStateException('Must apply \'com.android.application\' or \'com.android.library\' first!')
+        }
 
         project.afterEvaluate {
-
-            //只可以在 android application 或者 android lib 项目中使用
-            if (!project.android) {
-                throw new IllegalStateException('Must apply \'com.android.application\' or \'com.android.library\' first!')
-            }
-
-            //配置不能为空
-            if (project.apkdistconf.nameMap == null || project.apkdistconf.destDir == null) {
-                project.logger.info('apkdistconf conf should be set!')
-                return
-            }
-
-
-            Boolean isApp = true;
-            isApp = project['apkdistconf'].isApp
             Closure nameMap = project['apkdistconf'].nameMap
             String destDir = project['apkdistconf'].destDir
-
+            isApp = project['apkdistconf'].isApp
             //枚举每一个 build variant
             if (!isApp) {
+                println "当前项目不是APP独立项目，打包的APK将被输出到---->>$destDir" + nameMap("")
+//                project.logger.info("当前项目不是APP独立项目，打包的APK将被输出到---->>$destDir" + nameMap(""))
                 project.android.applicationVariants.all { variant ->
                     variant.outputs.each { output ->
                         File file = output.outputFile
                         output.outputFile = new File(destDir, nameMap(file.getName()))
                     }
                 }
+            } else {
+                println "当前项目是APP独立项目，打包的APK将被输出到---->>build/output/apk"
+//                project.logger.info("当前项目是APP独立项目，打包的APK将被输出到---->>build/output/apk")
             }
         }
     }
